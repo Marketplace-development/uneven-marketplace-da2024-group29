1a9ad0caf20a0e2bf2aa694f527322d95e8718f7
RikVandenBerge <rik1904@hotmail.com>
2024-11-20 12:56:43 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config: 
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rniucvwgcukfmgiscgzj:[DBS2024_Ugent]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.rniucvwgcukfmgiscgzj:DBS2024_Ugent@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
