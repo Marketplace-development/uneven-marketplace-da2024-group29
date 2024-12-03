94278d59c2d288bf06ee698654094751d37da865
mattiasverhaege <mattias.verhaege@ugent.be>
2024-12-03 18:56:48 +0100

    Nodig voor flash berichten

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
+import os #nodig om flash berichten te laten verschijnen
+
+
 class Config: 
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'  #eerste gedeelte nodig om Flash berichten te laten verschijnen
     
     SQLALCHEMY_DATABASE_URI = (
         'postgresql://postgres.rniucvwgcukfmgiscgzj:DBS2024_Ugent@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?sslmode=require'
