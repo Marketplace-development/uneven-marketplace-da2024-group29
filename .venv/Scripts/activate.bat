2522459b1d9f46b7795fc09f4bc1721e506399fb
MauritsOR <maurits.orourke@ugent.be>
2024-12-04 11:49:37 +0100

    venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rik19\OneDrive\Documenten\GitHub\uneven-marketplace-da2024-group29\.venv
+set VIRTUAL_ENV=c:\Users\mauri\Documents\GitHub\uneven-marketplace-da2024-group29\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
